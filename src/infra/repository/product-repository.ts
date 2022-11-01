756a9f7190120f770ce91f1cb48ede6418e9bc23
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-11-01 16:58:44 -0300

    feat: ensure ProductRepository creates a product

---
+++ src/infra/repository/product-repository.ts
@@ -0,0 +1,25 @@
+import Product from 'domain/entity/product'
+import ProductRepositoryInterface from 'domain/repository/product-repository-interface'
+import ProductModel from '../db/sequelize/model/product'
+
+export default class ProductRepository implements ProductRepositoryInterface {
+  async create(entity: Product): Promise<void> {
+    await ProductModel.create({
+      id: entity.id,
+      name: entity.name,
+      price: entity.price
+    })
+  }
+
+  async update(entity: Product): Promise<void> {
+    throw new Error("method not implemented")
+  }
+
+  async find(id: string): Promise<Product> {
+    throw new Error("method not implemented")
+  }
+
+  async findAll(): Promise<Product[]> {
+    throw new Error("method not implemented")
+  }
+}
