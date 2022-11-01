c12de5a141b41a06a8f3f0be6a4f9448cebad459
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-11-01 17:14:22 -0300

    feat: ensure ProductRepository find all products

--- src/infra/repository/product-repository.ts
+++ src/infra/repository/product-repository.ts
@@ -33,6 +33,9 @@ export default class ProductRepository implements ProductRepositoryInterface {
   }
 
   async findAll(): Promise<Product[]> {
-    throw new Error("method not implemented")
+    const productModels = await ProductModel.findAll()
+    return productModels.map((productModel) =>
+      new Product(productModel.id, productModel.name, productModel.price)
+    )
   }
 }
