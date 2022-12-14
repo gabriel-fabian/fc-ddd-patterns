1bfd2b6e337cc6135392a6095766f005217890ca
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-12-14 11:18:58 -0300

    feat: add events interfaces

---
+++ src/domain/event/@shared/index.ts
@@ -0,0 +1,3 @@
+export { default as EventDispatcherInterface } from './event-dispatcher-interface'
+export { default as EventHandlerInterface } from './event-handler-interface'
+export { default as EventInterface } from './event-interface'
