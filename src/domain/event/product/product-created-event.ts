740d9b14733a843bf1394b5905db1015871bf36e
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-12-14 13:24:13 -0300

    fix: typo in EventInterface

--- src/domain/event/product/product-created-event.ts
+++ src/domain/event/product/product-created-event.ts
@@ -3,6 +3,6 @@ import EventInterface from '@/domain/event/@shared/event-interface'
 export default class ProductCreatedEvent implements EventInterface {
   constructor (
     readonly eventData: any,
-    readonly dataTimeOccurred = new Date()
+    readonly dateTimeOccurred = new Date()
   ) {}
 }
