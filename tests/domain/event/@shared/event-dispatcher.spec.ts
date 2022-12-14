ea65206138f8a59d3ad83190e81c09952dadb1a1
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-12-14 11:28:59 -0300

    test: ensure EventDispatcher unregister an event handler

--- tests/domain/event/@shared/event-dispatcher.spec.ts
+++ tests/domain/event/@shared/event-dispatcher.spec.ts
@@ -12,4 +12,18 @@ describe('EventDispatcher test', () => {
     expect(eventDispatcher.getEventHandlers.ProductCreatedEvent.length).toBe(1)
     expect(eventDispatcher.getEventHandlers.ProductCreatedEvent[0]).toMatchObject(eventHandler)
   })
+
+  it('should unregister an event handler', () => {
+    const eventDispatcher = new EventDispatcher()
+    const eventHandler    = new SendEmailWhenProductIsCreatedHandler()
+
+    eventDispatcher.register('ProductCreatedEvent', eventHandler)
+
+    expect(eventDispatcher.getEventHandlers.ProductCreatedEvent[0]).toMatchObject(eventHandler)
+
+    eventDispatcher.unregister('ProductCreatedEvent', eventHandler)
+
+    expect(eventDispatcher.getEventHandlers.ProductCreatedEvent).toBeDefined()
+    expect(eventDispatcher.getEventHandlers.ProductCreatedEvent.length).toBe(0)
+  })
 })
